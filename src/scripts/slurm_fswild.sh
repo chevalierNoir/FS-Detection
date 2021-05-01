d5273ff0ccc1bfad8704ac074aaa6fe551003813
Bowen Shi <bshi@slurm.ttic.edu>
2021-04-30 23:18:06 -0500

    update

--- src/scripts/slurm_fswild.sh
+++ src/scripts/slurm_fswild.sh
@@ -6,7 +6,7 @@
 #SBATCH --output=slurm-%A.out
 
 for i in `seq 0 1000`;do
-    srun -p speech-cpu -N1 -n1 -c1 --exclusive ./preproc/pipeline.sh -d ./data/FWD/ -t ChicagoFSWild -j $i -s $1 &
+    srun -p speech-cpu -N1 -n1 -c1 --exclusive ./preproc/pipeline.sh -d ./data/fswild/ -t ChicagoFSWild -j $i -s $1 &
 done
 
 wait
--- src/scripts/slurm_fswildplus.sh
+++ src/scripts/slurm_fswildplus.sh
@@ -6,7 +6,7 @@
 #SBATCH --output=slurm-%A.out
 
 for i in `seq 0 1000`;do
-    srun -p speech-cpu -N1 -n1 -c1 --exclusive ./preproc/pipeline.sh -d ./data/PWD/ -t ChicagoFSWildPlus -j $i -s $1 &
+    srun -p speech-cpu -N1 -n1 -c1 --exclusive ./preproc/pipeline.sh -d ./data/fswildplus/ -t ChicagoFSWildPlus -j $i -s $1 &
 done
 
 wait
